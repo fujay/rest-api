d7ca7f929159899a626b5a3b8de8c6c7a0ad269b
Marcel Klamm <fujitsustar@hotmail.de>
2021-01-13 21:05:47 +0100

    Completed ToDoAppController.java with Spring Boot Default behavior

--- src/main/java/com/example/restapi/A01/controller/ToDoAppController.java
+++ src/main/java/com/example/restapi/A01/controller/ToDoAppController.java
@@ -1,18 +1,35 @@
 package com.example.restapi.A01.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController // Scanning looking for endpoints for rest api
-@RequestMapping(value = "/")
+@RequestMapping(value = "/todo")
 public class ToDoAppController {
 
+    private Map<Integer, String> todos = new HashMap<>();
+
     //@RequestMapping(method = RequestMethod.POST)
-    @PostMapping("todo")
+    @PostMapping()
     public void add(String todo) {
-        System.out.println(todo);
+        todos.put(1, todo);
+    }
+
+    @PutMapping()
+    public void update(Integer position, String todo) {
+        todos.put(position, todo);
+    }
+
+    @GetMapping
+    public String get() {
+        return todos.getOrDefault(1, "[]");
+    }
+
+    @DeleteMapping
+    public void delete(Integer position) {
+        todos.remove(position);
     }
 
 }
